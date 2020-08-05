253a2cb7e752496455ba68781db696564a7513e3
Guilherme Tinem <guizstinem@gmail.com>
2020-08-04 23:50:34 -0300

    ✨ Criado migrations de criacao das tabelas e corrigido as entidades das tabelas

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,37 @@
+import { Column, Entity, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn, ManyToMany, ManyToOne, JoinColumn } from 'typeorm';
+
+import Category from './Category';
+
+enum TypeTransaction {
+  Income = 'income',
+  Outcome = 'outcome'
+}
+
+@Entity()
 class Transaction {
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column()
+  type: TypeTransaction;
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
