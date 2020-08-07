972ba1b2e12744800637067f00813d8baca080c8
Guilherme Tinem <guizstinem@gmail.com>
2020-08-07 18:44:03 -0300

    ✨ Rota de delete transaction pronta

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,30 @@
-// import AppError from '../errors/AppError';
+import { isUuid } from 'uuidv4';
+import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    // Validate id
+    if(!isUuid(id)){
+      throw new AppError('Id not valid', 400);
+    }
+
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transactionToDelete = await transactionRepository.findOne({
+      id
+    });
+
+    if(!transactionToDelete) {
+      throw new AppError('Transaction not found', 404);
+    }
+
+    await transactionRepository.remove(transactionToDelete);
   }
 }
 
